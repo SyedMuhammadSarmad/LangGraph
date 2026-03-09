6672f8f750fe0445717ae66fec1bfa3d0d20ff7f
muhammadsarmad-cmd <muhammad.sarmad@tekrevol.com>
2026-03-10 00:35:03 +0500

    langgraph quickstart

---
+++ main.py
@@ -0,0 +1,35 @@
+from langgraph.graph import StateGraph, END
+from typing import TypedDict
+
+class AgentState(TypedDict):
+    messages: list[str]
+    step: int
+
+def node_a(state: AgentState) -> AgentState:
+    print("Node A running")
+    state["messages"].append("A was here")
+    state["step"] += 1
+    return state
+
+def node_b(state: AgentState) -> AgentState:
+    print("Node B running")
+    state["messages"].append("B was here")
+    return state
+
+def node_c(state:AgentState) -> AgentState:
+    print("Node C running")
+    state["messages"].append("C was here")
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node("node_a", node_a)
+graph.add_node("node_b", node_b)
+graph.add_node("node_c", node_c)
+
+graph.set_entry_point("node_a")
+graph.add_edge("node_a", "node_b")
+graph.add_edge("node_b", "node_c")
+graph.add_edge("node_c", END)
+app = graph.compile()
+result = app.invoke({"messages": [], "step": 0})
+print(result)
